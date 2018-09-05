f800762bec3860c2ba4d5caef8b53e25991d06d0
mrneo0nline <39259021+mrneo0nline@users.noreply.github.com>
2018-09-06 00:51:49 +0700

    Chapter 3-4 homework
    Chapter 3-4 homework

---
+++ JavaScripts/Chapter 3-4/js/ex1.js
@@ -0,0 +1,13 @@
+/* var $ = function(id){
+	return document.getElementById(id);
+}
+
+var ex1 = function(){
+	var a = prompt("Input number a:");
+	var b = prompt("Input number b:");
+	var c;
+	// alert("You inputted: a = " + a +", b = " + b);
+	c = a;
+	b = c;
+	a = b;
+} */
